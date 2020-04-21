b4b227a8965a6af5c53653a8b3a59e24159b61e0
Ruslan <ruslan.guskov.00@mail.ru>
2020-04-21 23:44:51 +0300

    Add class Snake

--- Program.cs
+++ Program.cs
@@ -33,6 +33,9 @@ namespace Snake
             lineL.Draw();
             lineR.Draw();
 
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
             Console.ReadLine();
         }
     }
