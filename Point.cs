a691207d37a5f1aa48b2636688c4efc1a0e55300
Ruslan <ruslan.guskov.00@mail.ru>
2020-04-24 18:58:27 +0300

    com

--- Point.cs
+++ Point.cs
@@ -23,10 +23,53 @@ namespace Snake
 
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x += offset;
+            }
+            else if(direction == Direction.LEFT)
+            {
+                x -= offset;
+            }
+            else if(direction == Direction.UP)
+            {
+                y -= offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y += offset;
+            }
+        }
+
+        internal void Clear()
+        {
+            this.sym = ' ';
+            Draw();
+        }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
